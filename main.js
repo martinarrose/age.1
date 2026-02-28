7489d57f9e95768a76095a77a079b693b2c636e7
martinarrose <mmartinarrose@gmail.com>
2026-02-28 11:02:05 +0100

    insert age + age2 and calculate the difference

---
+++ main.js
@@ -0,0 +1,6 @@
+let age = Number(prompt("inserisci la prima età:"));
+let age2 = Number(prompt("Inserisci la seconda età"));
+
+if (age < age2) {
+  alert(age + " e' minore di " + age2);
+} else if (age > age2) alert(age + " e' maggiore di " + age2);
